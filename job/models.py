cb62055954ee06cf061b3e9f7242678a3b46d25b
shanty-sys <shantysunuwar4@gmail.com>
2020-02-28 17:07:54 +0545

    model

--- job/models.py
+++ job/models.py
@@ -5,3 +5,11 @@ class job(models.Model):
     image = models.ImageField(upload_to = 'images/')
     summary = models.TextField(max_length = 200)
     vote = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.summary[0:2]
+
+    def summary(self):
+        return self.summary[0:3]
+
+
